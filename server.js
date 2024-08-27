c41baebdbd44c16fb8ce2b1659445558f05ca8f1
Allan <allanfelipedk@gmail.com>
2024-08-27 07:58:00 -0300

    npm install fastify

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-import { createServer } from "node:http";
+/*import { createServer } from "node:http";
 
-const server = createServer(() => {
-  console.log("oi");
+const server = createServer((request, response) => {
+  response.write("Hello World");
+
+  return response.end();
 });
 
 server.listen(3333);
+*/
+
+import { fastify } from "fastify";
+
+const server = fastify();
+
+server.get("/", () => {
+  return "Hello World!";
+});
+server.get("/node", () => {
+  return "Hello Node!";
+});
+server.get("/rockeseat", () => {
+  return "Hello Rockseate!";
+});
+
+server.listen({
+  port: 3333,
+});
